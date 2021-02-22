0fba6685c3f46c3da307139ce4ea574aaf8252b6
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2021-02-22 17:22:18 +1100

    landing page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Toggler from "./component/compare/toggle-chart/Toggler";
+import Banner from "./component/landing/banner/Banner";
 
 function App() {
   return (
     <div className="App">
-      <Toggler></Toggler>
+      <Banner></Banner>
+      {/* <Toggler></Toggler> */}
     </div>
   );
 }
---
+++ src/component/landing/banner/Banner.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import ".//Banner.scss";
+
+/* Banner Page, with button which navigates to the toggler page */
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>Master Hiragana & Katakana </h1>
+
+      <a className="btn" href="">
+        COMPARE
+      </a>
+    </div>
+  );
+}
+
+export default Banner;
